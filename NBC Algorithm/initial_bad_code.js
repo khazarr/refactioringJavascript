ffd9bf9971f8221b6d672444530f5e8fbaa7bab9
Karol Swieca <swieca.karol@gmail.com>
2018-09-04 17:45:15 +0200

    fixed variable name

--- NBC Algorithm/initial_bad_code.js
+++ NBC Algorithm/initial_bad_code.js
@@ -50,16 +50,16 @@ function setLabelProbabilities() {
 };
 
 function setChordCountsInLabels() {
-  songs.forEach(function (i) {
-    if (chordCountsInLabels[i[0]] === undefined) {
-      chordCountsInLabels[i[0]] = {};
+  songs.forEach(function (song) {
+    if (chordCountsInLabels[song[0]] === undefined) {
+      chordCountsInLabels[song[0]] = {};
     }
-    i[1].forEach(function (j) {
-      if (chordCountsInLabels[i[0]][j] > 0) {
-        chordCountsInLabels[i[0]][j] =
-          chordCountsInLabels[i[0]][j] + 1;
+    song[1].forEach(function (j) {
+      if (chordCountsInLabels[song[0]][j] > 0) {
+        chordCountsInLabels[song[0]][j] =
+          chordCountsInLabels[song[0]][j] + 1;
       } else {
-        chordCountsInLabels[i[0]][j] = 1;
+        chordCountsInLabels[song[0]][j] = 1;
       }
     });
   });
