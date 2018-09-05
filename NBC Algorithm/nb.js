9d5515a5934f9c4cab5a6d1a21254725bee1a6c5
Karol Swieca <swieca.karol@gmail.com>
2018-09-05 15:14:49 +0200

    refactor obj to map

--- NBC Algorithm/nb.js
+++ NBC Algorithm/nb.js
@@ -90,7 +90,7 @@ setProbabilityOfChordsInLabels();
 
 function classify(chords) {
   const smoothing = 1.01
-  var classified = {};
+  var classified = new Map();
   console.log(labelProbabilities);
   Object.keys(labelProbabilities).forEach(function (difficulty) {
     var first = labelProbabilities[difficulty] + smoothing;
@@ -101,7 +101,7 @@ function classify(chords) {
         first = first * (probabilityOfChordInLabel + smoothing);
       }
     });
-    classified[difficulty] = first;
+    classified.set(difficulty,first);
   });
   console.log(classified);
 };
