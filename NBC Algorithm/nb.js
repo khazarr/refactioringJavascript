e2ce0ba87344ddcf766919d932ec4c100c2fff85
Karol Swieca <swieca.karol@gmail.com>
2018-09-05 14:40:29 +0200

    removed dead code

--- NBC Algorithm/nb.js
+++ NBC Algorithm/nb.js
@@ -18,10 +18,9 @@ toxic = ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7',
 ];
 bulletproof = ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'];
 var songs = [];
-var labels = [];
 var allChords = new Set();
-var labelCounts = [];
-var labelProbabilities = [];
+var labelCounts = {};
+var labelProbabilities = {};
 var chordCountsInLabels = {};
 var probabilityOfChordsInLabels = {};
 const easy = 'easy';
@@ -30,7 +29,6 @@ const hard = 'hard';
 
 function train(chords, label) {
   songs.push([label, chords]);
-  labels.push(label);
   chords.forEach(chord => allChords.add(chord))
   if (Object.keys(labelCounts).includes(label)) {
     labelCounts[label] = labelCounts[label] + 1;
