e4ebb0463fc94c3737db94acdd1fffc5dea300b7
Karol Swieca <swieca.karol@gmail.com>
2018-09-08 11:50:44 +0200

    faking privacy with '_' before methods/fields

--- NBC Algorithm/nb_OOP_refactor.js
+++ NBC Algorithm/nb_OOP_refactor.js
@@ -1,6 +1,6 @@
 class Classifier {
   constructor() {
-    this.songList = {
+    this._songList = {
       allChords: new Set(),
       difficulties: {
         EASY: 'easy',
@@ -16,57 +16,57 @@ class Classifier {
         })
       }
     }
-    this.labelCounts = new Map()
-    this.labelProbabilities = new Map()
+    this._labelCounts = new Map()
+    this._labelProbabilities = new Map()
     this.chordCountsInLabels = new Map()
-    this.smoothing = 1.01
+    this._smoothing = 1.01
   }
   addSong(...songParameters) {
-    this.songList.addSong(...songParameters)
+    this._songList.addSong(...songParameters)
   }
-  chordCountForDifficulty(difficulty, testChord) {
-    return this.songList.songs.reduce((counter, song) => {
+  _chordCountForDifficulty(difficulty, testChord) {
+    return this._songList.songs.reduce((counter, song) => {
       if (song.difficulty === difficulty) {
         counter += song.chords.filter(chord => chord === testChord).length;
       }
       return counter;
     }, 0);
   }
-  likelihoodFromChord(difficulty, chord) {
-    return this.chordCountForDifficulty(difficulty, chord) /
-      this.songList.songs.length;
+  _likelihoodFromChord(difficulty, chord) {
+    return this._chordCountForDifficulty(difficulty, chord) /
+      this._songList.songs.length;
   }
-  valueForChordDifficulty(difficulty, chord) {
-    const value = this.likelihoodFromChord(difficulty, chord);
-    return value ? value + this.smoothing : 1;
+  _valueForChordDifficulty(difficulty, chord) {
+    const value = this._likelihoodFromChord(difficulty, chord);
+    return value ? value + this._smoothing : 1;
   }
   trainAll() {
-    this.songList.songs.forEach((song) => {
-      this.train(song.chords, song.difficulty);
+    this._songList.songs.forEach((song) => {
+      this._train(song.chords, song.difficulty);
     });
-    this.setLabelProbabilities();
+    this._setLabelProbabilities();
   }
-  train(chords, label) {
-    chords.forEach(chord => this.songList.allChords.add(chord));
-    if (Array.from(this.labelCounts.keys()).includes(label)) {
-      this.labelCounts.set(label, this.labelCounts.get(label) + 1);
+  _train(chords, label) {
+    chords.forEach(chord => this._songList.allChords.add(chord));
+    if (Array.from(this._labelCounts.keys()).includes(label)) {
+      this._labelCounts.set(label, this._labelCounts.get(label) + 1);
     } else {
-      this.labelCounts.set(label, 1);
+      this._labelCounts.set(label, 1);
     }
   }
-  setLabelProbabilities() {
-    this.labelCounts.forEach((_count, label) => {
-      this.labelProbabilities.set(label, this.labelCounts.get(label) /
-        this.songList.songs.length);
+  _setLabelProbabilities() {
+    this._labelCounts.forEach((_count, label) => {
+      this._labelProbabilities.set(label, this._labelCounts.get(label) /
+        this._songList.songs.length);
     });
   }
   classify(chords) {
     return new Map(Array.from(
-      this.labelProbabilities.entries()).map((labelWithProbability) => {
+      this._labelProbabilities.entries()).map((labelWithProbability) => {
       const difficulty = labelWithProbability[0];
       return [difficulty, chords.reduce((total, chord) => {
-        return total * this.valueForChordDifficulty(difficulty, chord);
-      }, this.labelProbabilities.get(difficulty) + this.smoothing)];
+        return total * this._valueForChordDifficulty(difficulty, chord);
+      }, this._labelProbabilities.get(difficulty) + this._smoothing)];
     }));
   }
 };
@@ -76,15 +76,15 @@ class Classifier {
 const wish = require('wish');
 describe('the file', () => {
   const classifier = new Classifier();
-  classifier.addSong('imagine', ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'], classifier.songList.difficulties.EASY)
-  classifier.addSong('somewhereOverTheRainbow', ['c', 'em', 'f', 'g', 'am'], classifier.songList.difficulties.EASY)
-  classifier.addSong('tooManyCooks', ['c', 'g', 'f'], classifier.songList.difficulties.EASY)
-  classifier.addSong('iWillFollowYouIntoTheDark', ['f', 'dm', 'bb', 'c', 'a', 'bbm'], classifier.songList.difficulties.MEDIUM)
-  classifier.addSong('babyOneMoreTime', ['cm', 'g', 'bb', 'eb', 'fm', 'ab'], classifier.songList.difficulties.MEDIUM)
-  classifier.addSong('creep', ['g', 'gsus4', 'b', 'bsus4', 'c', 'cmsus4', 'cm6'], classifier.songList.difficulties.MEDIUM)
-  classifier.addSong('paperBag', ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7', 'em7', 'a7', 'f7', 'b'], classifier.songList.difficulties.HARD)
-  classifier.addSong('toxic', ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'], classifier.songList.difficulties.HARD)
-  classifier.addSong('bulletproof', ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'], classifier.songList.difficulties.HARD)
+  classifier.addSong('imagine', ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'], classifier._songList.difficulties.EASY)
+  classifier.addSong('somewhereOverTheRainbow', ['c', 'em', 'f', 'g', 'am'], classifier._songList.difficulties.EASY)
+  classifier.addSong('tooManyCooks', ['c', 'g', 'f'], classifier._songList.difficulties.EASY)
+  classifier.addSong('iWillFollowYouIntoTheDark', ['f', 'dm', 'bb', 'c', 'a', 'bbm'], classifier._songList.difficulties.MEDIUM)
+  classifier.addSong('babyOneMoreTime', ['cm', 'g', 'bb', 'eb', 'fm', 'ab'], classifier._songList.difficulties.MEDIUM)
+  classifier.addSong('creep', ['g', 'gsus4', 'b', 'bsus4', 'c', 'cmsus4', 'cm6'], classifier._songList.difficulties.MEDIUM)
+  classifier.addSong('paperBag', ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7', 'em7', 'a7', 'f7', 'b'], classifier._songList.difficulties.HARD)
+  classifier.addSong('toxic', ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'], classifier._songList.difficulties.HARD)
+  classifier.addSong('bulletproof', ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'], classifier._songList.difficulties.HARD)
 
   classifier.trainAll()
 
@@ -103,8 +103,8 @@ describe('the file', () => {
   })
 
   it('label probabilities', () => {
-    wish(classifier.labelProbabilities.get('easy') === 0.3333333333333333);
-    wish(classifier.labelProbabilities.get('medium') === 0.3333333333333333);
-    wish(classifier.labelProbabilities.get('hard') === 0.3333333333333333);
+    wish(classifier._labelProbabilities.get('easy') === 0.3333333333333333);
+    wish(classifier._labelProbabilities.get('medium') === 0.3333333333333333);
+    wish(classifier._labelProbabilities.get('hard') === 0.3333333333333333);
   });
 });
