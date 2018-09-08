2fc2021a5a339fa6bfecc6cddbc1780ac8782cc1
Karol Swieca <swieca.karol@gmail.com>
2018-09-08 12:23:52 +0200

    lookup() method added

--- Chapter 9/word.js
+++ Chapter 9/word.js
@@ -5,6 +5,13 @@ class Word {
   count() {
     return this.word.length;
   };
+  lookUp() {
+    if (this instanceof JapaneseWord) {
+      return `http://jisho.org/search/${this.word}`;
+    } else {
+      return `https://en.wiktionary.org/wiki/${this.word}`;
+    }
+  }
 };
 class EnglishWord extends Word {};
 class JapaneseWord extends Word {};
@@ -12,5 +19,7 @@ const japaneseWord = new JapaneseWord("犬");
 const englishWord = new EnglishWord("dog");
 console.log(japaneseWord.count());
 console.log(japaneseWord.word);
+console.log(japaneseWord.lookUp())
 console.log(englishWord.count());
 console.log(englishWord.word);
+console.log(englishWord.lookUp())
