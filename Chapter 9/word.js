e23e6f3f1077397746ea8c3902933e43ebb9e5e6
Karol Swieca <swieca.karol@gmail.com>
2018-09-10 19:29:37 +0200

    object literals

--- Chapter 9/word.js
+++ Chapter 9/word.js
@@ -1,20 +1,27 @@
 class Word {
-  constructor(word) {
-    this.word = word;
-  };
+  constructor(word, language) {
+    this.word = word
+    this.language = language
+  }
   count() {
     return this.word.length;
   };
   lookUp() {
-    if (this instanceof JapaneseWord) {
-      return `http://jisho.org/search/${this.word}`;
-    } else {
-      return `https://en.wiktionary.org/wiki/${this.word}`;
-    }
+    return this.lookUpUrl + this.word;
   }
 };
-class EnglishWord extends Word {};
-class JapaneseWord extends Word {};
+class EnglishWord extends Word {
+  constructor(word) {
+    super(word, "English");
+    this.lookUpUrl = 'https://en.wiktionary.org/wiki/';
+  }
+};
+class JapaneseWord extends Word {
+  constructor(word) {
+    super(word, "Japanese");
+    this.lookUpUrl = 'http://jisho.org/search/';
+  }
+};
 const japaneseWord = new JapaneseWord("犬");
 const englishWord = new EnglishWord("dog");
 console.log(japaneseWord.count());
@@ -23,3 +30,26 @@ console.log(japaneseWord.lookUp())
 console.log(englishWord.count());
 console.log(englishWord.word);
 console.log(englishWord.lookUp())
+
+
+const wish = require('wish');
+const deepEqual = require('deep-equal');
+// interfaces tests
+wish(japaneseWord.word === "犬");
+wish(japaneseWord.lookUp() === "http://jisho.org/search/犬");
+wish(japaneseWord.count() === 1);
+wish(englishWord.word === "dog");
+wish(englishWord.lookUp() === "https://en.wiktionary.org/wiki/dog");
+wish(englishWord.count() === 3);
+// internals tests
+wish(typeof japaneseWord === 'object');
+wish(typeof JapaneseWord === 'function');
+wish(japaneseWord instanceof JapaneseWord);
+wish(japaneseWord instanceof Word);
+wish(!(JapaneseWord instanceof Word));
+wish(japaneseWord.constructor === JapaneseWord);
+wish(Object.getPrototypeOf(JapaneseWord) === Word);
+// sketchy bits
+wish(deepEqual(Object.getPrototypeOf(japaneseWord), {}));
+console.log(Object.getPrototypeOf(japaneseWord));
+// reports JapaneseWord {}
