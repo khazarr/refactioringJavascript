02f9ebb25e08329b05ff587c75ce9cee998f7fda
Karol Swieca <swieca.karol@gmail.com>
2018-09-11 20:35:24 +0200

    conditional moved to class

--- OOP JS Patterns/templateMethod.js
+++ OOP JS Patterns/templateMethod.js
@@ -1,21 +1,23 @@
 class Person {
-  constructor(binaryKnower) {
-    this.binaryKnower = binaryKnower;
-  };
+  constructor(typeOfPerson) {
+    this.typeOfPerson = typeOfPerson;
+  }
   whatIs(number) {
-    return number
+    return number;
   };
   whatIsInBinary(number) {
-    return Number('0b' + number)
+    return Number('0b' + number);
+  };
+  log(number) {
+    if (this.typeOfPerson === "binary knower") {
+      console.log(this.whatIsInBinary(10));
+    } else {
+      console.log(this.whatIs(10));
+    }
   };
 };
-const personOne = new Person(true);
-const personTwo = new Person(false);
-
+const personOne = new Person("binary knower");
+const personTwo = new Person("binary oblivious");
 [personOne, personTwo].forEach(person => {
-  if (person.binaryKnower) {
-    console.log(person.whatIsInBinary(10));
-  } else {
-    console.log(person.whatIs(10));
-  }
-})
+  person.log(10)
+});
