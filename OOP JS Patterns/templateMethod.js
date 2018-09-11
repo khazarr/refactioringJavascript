94a7c6b3dfde9f4164991b00d681033546f9cd2d
Karol Swieca <swieca.karol@gmail.com>
2018-09-11 20:38:01 +0200

    template method final

--- OOP JS Patterns/templateMethod.js
+++ OOP JS Patterns/templateMethod.js
@@ -1,23 +1,18 @@
 class Person {
-  constructor(typeOfPerson) {
-    this.typeOfPerson = typeOfPerson;
-  }
-  whatIs(number) {
-    return number;
-  };
-  whatIsInBinary(number) {
-    return Number('0b' + number);
-  };
   log(number) {
-    if (this.typeOfPerson === "binary knower") {
-      console.log(this.whatIsInBinary(10));
-    } else {
-      console.log(this.whatIs(10));
-    }
+    console.log(this.whatIs(number));
   };
 };
-const personOne = new Person("binary knower");
-const personTwo = new Person("binary oblivious");
-[personOne, personTwo].forEach(person => {
-  person.log(10)
-});
+class BinaryKnower extends Person {
+  whatIs(number) {
+    return Number('0b' + number)
+  };
+};
+class BinaryOblivious extends Person {
+  whatIs(number) {
+    return number
+  };
+};
+const personOne = new BinaryKnower();
+const personTwo = new BinaryOblivious();
+[personOne, personTwo].forEach(person => person.log(10));
